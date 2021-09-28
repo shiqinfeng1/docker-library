e8cf2a15785726b3fb10d9c165dfe569c5a95a84
shiqinfeng1 <150627601@qq.com>
2021-09-28 17:12:20 +0800

    update

--- download.sh
+++ download.sh
@@ -8,8 +8,8 @@ ImagesList=$CurrentDIR/image-list.txt
 for image in $(<${ImagesList}); do
     array=(${image//:/ })
     image0=${array[0]}
-    DIR_NAME="$(echo ${image0} | sed s@"/"@"-"@g)"
-    docker pull registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image0:latest
-    docekr tag  registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image0:latest $image
-    docker rmi  registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image0:latest
+    image2="$(echo ${image0} | sed s@"/"@"-"@g)"
+    docker pull registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image2:latest
+    docekr tag  registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image2:latest $image
+    docker rmi  registry.cn-shanghai.aliyuncs.com/shiqinfeng1/$image2:latest
 done
